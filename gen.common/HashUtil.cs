4b0e5862b40d097679d01917b2c427e843e4b752
Gleb Gorev <gorev.gb@outlook.com>
2021-12-15 22:53:25 +0400

    new utility

---
+++ gen.common/HashUtil.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace gen.common
+{
+    public static class HashUtil
+    {
+        public static string ComputeHash(string input)
+        {
+            using var sha = SHA256.Create();
+            var hash = sha.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            var sBuilder = new StringBuilder();
+
+            foreach (var b in hash)
+            {
+                sBuilder.Append(b.ToString("x2"));
+            }
+
+            return sBuilder.ToString();
+        }
+    }
+}
