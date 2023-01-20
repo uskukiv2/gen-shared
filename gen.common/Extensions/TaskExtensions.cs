d9347229eea1cc5d6693bf5d83ad6822bd5c6579
Gleb Gorev <gorev.gb@outlook.com>
2023-01-20 20:12:01 +0400

    FED-29 Add base for Menu service

---
+++ gen.common/Extensions/TaskExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace gen.common.Extensions;
+
+public static class TaskExtensions
+{
+    public static async Task Forget(this Task task)
+    {
+         await task.ConfigureAwait(false);
+    }
+}
